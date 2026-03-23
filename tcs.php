80232b361cd8e6a818aa10dc4d959a8f390494b7
idol ♪ <ozkancakirgoz2007@gmail.com>
2026-03-23 10:18:13 +0000

    Add files via upload

---
+++ tcs.php
@@ -0,0 +1,26 @@
+<?php
+session_start()
+?>
+<!DOCTYPE html>
+<html lang="en-uk">
+    <head>
+        <title>Greenfield Local Hub</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="defaultstyle.css">
+    </head>
+
+    <body>
+        <?php 
+        include ("header.php")
+        ?>
+
+        <h1 class="manrope-title">Welcome to Greenfield Local Hub</h1>
+        <p class="manrope-body"> Filler Body </p>
+        
+        <?php
+        include ("footer.php")
+        ?>
+    </body>
+
+</html>
